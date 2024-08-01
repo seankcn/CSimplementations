6ccad89303cc867fbdf3de2aed87609c40ab62a4
Sean Kilcullen <sean.kilcullen@ucdconnect.ie>
2024-08-01 13:25:53 +0100

    Completed Binary Search implementation

--- C/binarySearch.c
+++ C/binarySearch.c
@@ -1,3 +1,16 @@
 
-int binary_Search(int* array, int len){
+int binary_Search(int* array, int len, int target){
+    int start = 0;
+    int end = len-1;
+    while(start <= end){
+        int pos = start + ((end-start) >> 1);
+        if(array[pos] == target){
+            return pos;
+        }else if(array[pos] > target){
+            end = pos-1;
+        }else{
+            start = pos+1;
+        }
+    }
+    return -1;
 }

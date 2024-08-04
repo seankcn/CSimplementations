cbd8591d1816c902138a08940224427d591d6715
Sean Kilcullen <sean.kilcullen@ucdconnect.ie>
2024-08-04 14:23:51 +0100

    Completed Quicksort implementation

--- C/quicksort.c
+++ C/quicksort.c
@@ -1,3 +1,31 @@
 
-void quicksort(int* array, int len){
+void swap(int* a, int* b){
+    *a ^= *b;
+    *b ^= *a;
+    *a ^= *b;
+}
+int Hoare(int* array, int start, int end, int pivot){
+    int i = start;
+    int j = end;
+    for(;;){
+        while(i < end && array[i] < pivot){
+            i++;
+        }
+        while(j > start && array[j] >= pivot){
+            j--;
+        }
+        if(i >= j){
+            return j;
+        }
+        swap(array+i, array+j);
+    }
+}
+void quicksort(int* array, int start, int end){
+    if(end-start <= 1){
+        return;
+    }
+    int pivot = array[start + ((end-start)/2)];
+    int mid = Hoare(array, start, end, pivot);
+    quicksort(array, start, mid);
+    quicksort(array, mid+1, end);
 }
